8e7cd5509d69a80f5ee05652e08db9b6d4940488
ashutoshskhandelwal <ashutosh.khandelwal78@gmail.com>
2017-06-21 13:22:27 -0400

    Final Commit

---
+++ invertedIndexWithHadoop/src/finalProject/InvertedIndex.java
@@ -0,0 +1,107 @@
+package finalProject;
+
+
+import java.util.*;
+import java.io.*;
+ 
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.io.*;
+ 
+public class InvertedIndex {
+ 
+            //mapper code
+            public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+                        private Text word = new Text();
+                        private Text location = new Text();
+                        private HashSet<String> dict = new HashSet<String>();
+                        private final static int numCommonWords = 6000;
+                       
+                        public void map(LongWritable key, Text value,OutputCollector<Text, Text> output, Reporter reporter)throws IOException {
+                                    String line = value.toString();
+                                    StringTokenizer tokenizer = new StringTokenizer(line);
+                                    FileSplit fileSplit = (FileSplit)reporter.getInputSplit();
+                                    String fileName = fileSplit.getPath().getName();
+                                    location.set(fileName);
+                                    if (dict.isEmpty()){
+                                                readDict();
+                                    }
+                                    while (tokenizer.hasMoreTokens()){
+                                                String word_str = scrub(tokenizer.nextToken());
+                                                if (!dict.contains(word_str)) {
+                                                            word.set(word_str);
+                                                            output.collect(word, location);
+                                                }
+                                    }
+                        }
+                        private String scrub(String noisyword) {                                 
+                                    return noisyword.toLowerCase().replaceAll("\\pP|\\pS", "");
+                        }
+                        private void readDict() throws IOException {
+                                    Configuration conf = new Configuration();
+                                    FileSystem fs = FileSystem.get(conf);
+                                    InputStream in = null;
+                                    try {
+                                                int countNum = 0;
+                                                in = fs.open(new Path("/dict"));                                              
+                                                Scanner sc = new Scanner(in);                                    
+                                                while (sc.hasNext() && countNum < numCommonWords) {                                                 
+                                                            String dictword = scrub(sc.next());
+                                                            dict.add(dictword);
+                                                            countNum ++;
+                                                }                                             
+                                    }finally {
+                                                IOUtils.closeStream(in);
+                                    }
+                        }
+            }
+            //Reducer code
+            public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+                        private Text index = new Text();
+                        public void reduce(Text key, Iterator<Text> value,OutputCollector<Text, Text> output, Reporter reporter)throws IOException {
+                                    StringBuffer tmp = new StringBuffer();
+                                    String pre = new String();
+                                    boolean first = true;
+                                    while (value.hasNext()){
+                                                String tmp2 = value.next().toString();
+                                                if (!tmp2.equals(pre)){
+                                                            pre = tmp2;
+                                                            if (first){
+                                                                        first = false;
+                                                            }else {
+                                                                        tmp.append(',');
+                                                            }
+                                                            tmp.append(tmp2);
+                                                }
+                                    }
+                                    index.set(tmp.toString());
+                                    output.collect(key, index);
+                        }
+            }
+           
+            // main method 
+            public static void main(String[] args) throws IOException {
+                        // TODO Auto-generated method stub
+                        JobConf conf = new JobConf(InvertIndex.class);
+                        conf.setJobName("invertindex"); //job name assigned
+                       
+                        conf.setOutputKeyClass(Text.class);
+                        conf.setOutputValueClass(Text.class);
+                       
+                        conf.setMapperClass(Map.class);
+                        conf.setCombinerClass(Reduce.class);
+                        conf.setReducerClass(Reduce.class);
+                       
+                        conf.setInputFormat(TextInputFormat.class);
+                        conf.setOutputFormat(TextOutputFormat.class);
+                       
+                        FileSystem.get(conf).delete(new Path(args[1]), true);
+                        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+                        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+                       
+                        JobClient.runJob(conf);
+            }
+ 
+}
---
+++ invertedIndexWithHadoop/src/finalProject/Word.java
@@ -0,0 +1,58 @@
+package finalProject;
+
+import java.util.*;
+import java.io.*;
+ 
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.io.*;
+ 
+public class QueryWord {       
+            public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+                        private JobConf conf; 
+                        @Override
+                        public void configure(JobConf conf) { this.conf = conf; }                      
+                        public void map(LongWritable key, Text value,OutputCollector<Text, Text> output, Reporter reporter)throws IOException {
+                                    String line = value.toString();
+                                    String[] tokens = line.split("\t");
+                                    if (tokens[0].equals(conf.get("keyword")))                             
+                                    	output.collect(new Text(tokens[0]), new Text(tokens[1]));
+                        }
+            }
+            //main method
+            public static void main(String[] args) throws IOException {
+                        JobConf conf = new JobConf(QueryWord.class);
+                        FileSystem fs = FileSystem.get(conf);
+ 
+                        conf.setJobName("queryword");
+                        conf.setMapperClass(Map.class);
+                       
+                        conf.setInputFormat(TextInputFormat.class);
+                        conf.setOutputFormat(TextOutputFormat.class);
+                       
+                        conf.setOutputKeyClass(Text.class);
+                        conf.setOutputValueClass(Text.class);
+ 
+                        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+                        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+                       
+                        // read the keyword
+                        Scanner s = new Scanner(System.in);
+                        System.out.println("Please input the word to query:");
+                        String word = s.next();
+                        conf.set("keyword", word);
+ 
+                        fs.delete(new Path(args[1]), true);
+ 
+                        JobClient.runJob(conf);
+ 
+                        // print the result
+                        System.out.println("The result is:");
+                        s = new Scanner(fs.open(new Path(args[1] + "/part-00000")));
+                        s.next();
+                        System.out.println(s.next());
+            }
+ 
+}
